f4d89471f478837e0db1ef94a9911255ff935a03
bona <sbai1@stevens.edu>
2016-06-06 17:36:19 -0400

    heading

--- Explo_newRaycase_36headings.m
+++ Explo_newRaycase_36headings.m
@@ -26,45 +26,22 @@ SensorRange = 80;
 SearchRange = SensorRange/5;
 RobotStep = SensorRange/2;
 RobotStep_long=SensorRange;
-RndSet = sobolset(2);
-RndSet = ceil(RndSet(100:100000,:).*2*SensorRange);
-RndPat = zeros(SensorRange*2,SensorRange*2,'uint32');
-for i=1:length(RndSet)
-    if(RndPat(RndSet(i,1),RndSet(i,2)) == 0)
-        RndPat(RndSet(i,1),RndSet(i,2)) = i;
-    end
-end
+
 % setup GP Regression
 
 
 [ OP_MAP, cur_free ] = InverseSensorModel( RoboPosi, SensorRange, OP_MAP, Resolution, map); 
+Entropy_1 = length(find(OP_MAP==127));
+
 
 tic
 RoboPosi = [118 135 90]'; % [x  y  yaw_deg]
 [ OP_MAP, cur_free ] = InverseSensorModel( RoboPosi, SensorRange, OP_MAP, Resolution, map); 
+Entropy_2 = length(find(OP_MAP==127))
 
 
-SelPat = zeros(SensorRange*2,SensorRange*2,'uint32');
-offset = [RoboPosi(1)-SensorRange RoboPosi(2)-SensorRange ];
-for i=1:length(cur_free)
-    SelPat(cur_free(i,1)-offset(1),cur_free(i,2)-offset(2)) = 1;
-end
 
-SelPts = RndPat.*SelPat;
-figure(3); imshow(SelPts,[0 1]);
 
-% SelPts(SelPts==0) = max(SelPts(:)) + 1;
-SelPts(SelPts==0) = inf;
-% SelPts = SelPts - min(SelPts(:)) + 1;
-[Xs, Xind] = sort(SelPts(:));
-
-Candidates = zeros(10,2);
-for i = 1:10
-    Candidates(i,1) = rem(Xind(i),size(SelPts,2));
-    Candidates(i,2) = ceil(Xind(i)/size(SelPts,2));
-    SelPts(Candidates(i,1),Candidates(i,2))
-end
-% [I,J] = find(SelPts>-inf,10,'last')
 
 toc
 figure(2); imshow(OP_MAP,[0 255]);
